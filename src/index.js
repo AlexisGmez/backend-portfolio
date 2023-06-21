887e50575e72d1370ef1a3b4a8f89cf6becbed32
AlexisGmez <alexisgmez65@gmail.com>
2023-06-21 08:37:16 -0600

    feat: adding script start

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { sequelize } from "./database/database.js";
 const main = async () => {
   try {
     await sequelize.sync({ force: false });
-
-    app.listen(4000);
-    console.log("listen on port 4000");
+    const port = process.env.PORT;
+    app.listen(port);
+    console.log("listen on port " + port);
   } catch (error) {
     console.log("fallo de conexion", error);
   }
